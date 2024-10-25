a217bc2abfa02454d28660e1cd7824d4084d95e0
ArkaaKai <manrayxd697@gmail.com>
2024-10-25 14:17:37 +0700

    first commit

---
+++ resources/views/kategori/detail.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                <div class="table-responsive"> 
+                    <div class="card-title mt-2 mb-2">
+                        <h5>Sains</h5>
+                        <p class="text-muted">Buku Yang Masuk Kategori Sains :</p>
+
+                    </div>
+                    <table class="table table-hover"> 
+                        <thead>
+                             <th>Nama Kategori</th> 
+                             <th>Pilihan</th>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
